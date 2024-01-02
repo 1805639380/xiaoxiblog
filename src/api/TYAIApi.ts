1555bf0f499d4c0a51bc316e5c181117ecaacc48
Hololo <1805639380@qq.com>
2024-01-02 15:36:46 +0800

    fix: 修复权限校验问题

--- src/api/TYAIApi.ts
+++ src/api/TYAIApi.ts
@@ -1,10 +1,13 @@
 const AI_BASEURL = import.meta.env.VITE_API_HOST + "/api/v1/ai";
 
 export const getAIReply = (prompt: string, model: string) => {
+  const cookieToken = useCookie("token");
   return fetch(AI_BASEURL, {
+    credentials: "include",
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Authorization: "Bearer " + cookieToken.value,
     },
     body: JSON.stringify({
       model,

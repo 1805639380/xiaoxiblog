ea5f5a9aa45184c79aa497ead008c4d74f3f09cf
Hololo <1805639380@qq.com>
2024-01-21 11:17:13 +0800

    refactor: 重构动画实现

--- src/components/BackTop.vue
+++ src/components/BackTop.vue
@@ -2,9 +2,14 @@
   <div id="backTop">
     <div
       class="back"
-      :class="{ isShow: windowScrollTop > 200 }"
+      :class="{
+        isShow: windowScrollTop > 200,
+        backTopFadeOut: !isFirstScroll && windowScrollTop < 200,
+      }"
       @click="backTopFun"
-    ></div>
+    >
+      <img src="~assets/img/scroll.png" alt="" />
+    </div>
   </div>
 </template>
 
@@ -12,20 +17,25 @@
 let windowScrollTop = ref(0);
 
 let isStickTop = ref(true);
+const isFirstScroll = ref(true);
 
 let backTopFun = ref(() => {});
 
-onMounted(() => {
+const backTopDown = (e) => {
+  //滚动条高度大于10的时候导航栏固定
   windowScrollTop.value = document.documentElement.scrollTop;
 
-  const backTopDown = (e) => {
-    //滚动条高度大于10的时候导航栏固定
-    windowScrollTop.value = document.documentElement.scrollTop;
+  windowScrollTop.value > 10
+    ? (isStickTop.value = true)
+    : (isStickTop.value = false);
 
-    windowScrollTop.value > 10
-      ? (isStickTop.value = true)
-      : (isStickTop.value = false);
-  };
+  if (isFirstScroll.value && windowScrollTop.value > 200) {
+    isFirstScroll.value = false;
+  }
+};
+
+onMounted(() => {
+  windowScrollTop.value = document.documentElement.scrollTop;
 
   window.addEventListener("scroll", backTopDown);
 
@@ -47,28 +57,32 @@ onMounted(() => {
     });
   };
 });
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", backTopDown);
+});
 </script>
 
 <style scoped>
-.isShow {
-  background-position-y: -35vh !important;
-  animation: backTop 1.6s linear infinite;
-  height: 100vh !important;
-}
-
 #backTop .back {
   position: fixed;
   right: 1.875rem;
-  height: 0vh;
-  width: 80px;
-  background: url(assets/img/scroll.png) no-repeat center;
-  background-position-y: -1050px;
   z-index: 9;
-  transition: all 0.3s linear;
+  transform: translate3d(0, -1050px, 0);
   cursor: pointer;
 }
 
-@keyframes backTop {
+#backTop .isShow {
+  transform: translate3d(0, 0, 0);
+  animation: backTopFadeIn 0.3s linear forwards,
+    upAndDownAnimate 1.6s 0.3s linear infinite;
+}
+
+#backTop .backTopFadeOut {
+  animation: backTopFadeOut 0.3s linear forwards;
+}
+
+@keyframes upAndDownAnimate {
   0%,
   100% {
     transform: translateY(-10px);
@@ -82,6 +96,24 @@ onMounted(() => {
     -webkit-transform: translateY(0px);
     -moz-transform: translateY(0px);
     -ms-transform: translateY(0px);
+  }
+}
+@keyframes backTopFadeIn {
+  0% {
+    transform: translateY(-1050px);
+  }
+
+  100% {
+    transform: translateY(0);
+  }
+}
+@keyframes backTopFadeOut {
+  0% {
+    transform: translateY(0);
+  }
+
+  100% {
+    transform: translateY(-1050px);
   }
 }
 @media screen and (max-width: 768px) {
